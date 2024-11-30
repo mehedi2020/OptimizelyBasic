85f2553616bb2c802f03272b71267c6aedddc4d9
Mehedi Hasan <mehedi4055@gmail.com>
2024-11-30 20:06:02 +1000

    Update

---
+++ src/Project/Basic/Code/Program.cs
@@ -0,0 +1,11 @@
+namespace Project.Basic;
+
+public class Program
+{
+    public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureCmsDefaults()
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+}
